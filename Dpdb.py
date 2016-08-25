80f4e3aea39d451b3026e2b0ad8ebc7932926f42
luobingbing3 <luolingbing@luolingbingdeMacBook-Pro.local>
2016-08-25 22:18:54 +0800

    Merge2016.08.25

--- Dpdb.py
+++ Dpdb.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 from flaskext.mysql import MySQL
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory,jsonify
 from InsertDataFromExcel import insertDataFromExcel
 import os
+import pygal
+import dropDown
+import graphing
 
 app = Flask(__name__)
 
 mysql = MySQL()
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'      # 输入自己数据库的用户名
+app.config['MYSQL_DATABASE_USER'] = 'root'  # 输入自己数据库的用户名
 app.config['MYSQL_DATABASE_PASSWORD'] = '1111'  # 输入自己数据库用户名的密码
 app.config['MYSQL_DATABASE_DB'] = 'Dpdb'  # 我们要使用的数据库名字
-app.config['MYSQL_DATABASE_HOST'] = 'localhost' # 要使用数据库的网址
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'  # 要使用数据库的网址
+app.config['MYSQL_DATABASE_CHARSET'] = 'utf8'
 mysql.init_app(app)
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['xls', 'xlsx'])
@@ -28,7 +32,7 @@ def main():
 
 
 @app.route('/upload_homepage')
-def homepage():
+def upload_homepage():
     return render_template('upload_homepage.html')
 
 
@@ -66,10 +70,44 @@ def upload_succeeded():
         return render_template('upload_error.html', error=e)
 
 
+
+@app.route('/filterStudent', methods=['GET', 'POST'])
+def filterStudent():
+    current_coach = request.args.get('choice', 0, type=int)
+    try:
+        db = mysql.connect()
+        cursor = db.cursor()
+        try:
+            student_list = dropDown.filter(current_coach, cursor)
+            return jsonify(student_list=student_list)
+        except:
+            print "filterStudent - step2 failed."
+            print "Error: unable to fetch data"
+    except Exception as e:
+        print "filterStudent - step1 failed."
+        return str(e)
+    finally:
+        cursor.close()
+        db.close()
+
+
 @app.route('/showResult')
 def showResult():
-    return render_template('showResult.html')
+    # show the init result. ave-coach and ave-student
+    try:
+        db = mysql.connect()
+        cursor = db.cursor()
+        try:
+            coach_info, student_info = dropDown.init(cursor)
+            return render_template('showResult.html', coach_info=coach_info, student_info=student_info)
 
+        except:
+            print "Error: unable to fetch data"
+    except Exception as e:
+        return str(e)
+    finally:
+        cursor.close()
+        db.close()
 
 if __name__ == '__main__':
     app.run()
---
+++ dropDown.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+def init(cursor):
+    """get the default dropdown-select option results(id & name_) from mysql when open the whoResult.html"""
+    coach_info = []
+    student_info = []
+    sql1 = "select id, name_ from coach"
+    sql2 = "select id, name_ from student"
+    cursor.execute(sql1)
+    results = cursor.fetchall()
+    for row in results:
+        coach_info.append((row[0], row[1]))
+    cursor.execute(sql2)
+    results = cursor.fetchall()
+    for row in results:
+        student_info.append((row[0], row[1]))
+    return coach_info, student_info
+
+def filter(current_coach, cursor):
+    """get the selected dropdown option results(id & name_) from mysql after new choice made."""
+    student_list = []
+    sql = "select id, name_ from student "
+    if current_coach != 0:
+        sql += "where coach_id = " + str(current_coach)
+        print sql
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for row in results:
+        student_list.append((row[0], row[1]))
+    print "filterStudent success!"
+    return student_list
