b01fabfd21a251a7ecee9b517a194f0cb2c1b971
luobingbing3 <luolingbing@luolingbingdeMacBook-Pro.local>
2016-09-11 11:33:11 +0800

    Final_Version 1.3
    1. 筛选逻辑重新修改，只能通过某一个教练进行筛选学员，可显示该教练的某个或者所有学员的信息。
    	2. 新增显示课程条目数的列。
    	3. id列隐藏完成。
    	4. 表格高度，宽度均能随窗口大小自由变化。

--- showAndDelete.py
+++ showAndDelete.py
@@ -6,6 +6,10 @@ def selectItemsFromLesson(cursor, coach_id, student_id):
     if coach_id != 0 and student_id != 0:
         sql += 'where coach_id = ' + str(coach_id) + ' and student_id = ' + str(student_id)
         print sql
+    if coach_id != 0 and student_id == 0:
+        sql += 'where coach_id = ' + str(coach_id)
+    if student_id == -1:
+        return 0
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
